25851d104a3631fb8d0e4574c3a77fad0e872772
Junior <juancarlosamarilla@proton.me>
2024-11-01 15:00:25 +0000

    add generic views

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
-from django.shortcuts import render
-
-# Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingItemView(generics.ListCreateAPIView):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated] 
